88b0d94ce7d9a9aee312cf4a13b2938f4f110ffa
Bateristico <camilo.saa@gmail.com>
2021-08-31 21:29:27 +0200

    finishing form validation

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,20 +1,35 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: !firstName.isValid }"
+      @blur="clearValidity('firstName')"
+    >
       <label for="firstname">FirstName</label>
       <input type="text" id="firstname" v-model.trim="firstName.val" />
+      <p v-if="!firstName.isValid">First name must not be empty</p>
     </div>
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: !lastName.isValid }"
+      @blur="clearValidity('lastName')"
+    >
       <label for="lastname">Lastname</label>
       <input type="text" id="lastname" v-model.trim="lastName.val" />
+      <p v-if="!lastName.isValid">Last name must not be empty</p>
     </div>
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: !lastName.isValid }"
+      @blur="clearValidity('description')"
+    >
       <label for="description">Description</label>
       <textarea
         id="description"
         rows="5"
         v-model.trim="description.val"
       ></textarea>
+      <p v-if="!description.isValid">Description must not be empty</p>
     </div>
     <div class="form-control">
       <label for="rate">Hourly Rate</label>
@@ -45,6 +60,7 @@
         <label for="career">Career Advisory</label>
       </div>
     </div>
+    <p v-if="!formIsValid">Please fix the errors and submit again</p>
     <base-button>Register</base-button>
   </form>
 </template>
@@ -78,6 +94,9 @@ export default {
     };
   },
   methods: {
+    clearValidity(input) {
+      this[input].isValid = true;
+    },
     validateForm() {
       this.formIsValid = true;
       if (this.firstName.val === '') {
@@ -112,11 +131,11 @@ export default {
       }
 
       const formData = {
-        first: this.firstName,
-        last: this.lastName,
-        desc: this.description,
-        rate: this.rate,
-        areas: this.areas
+        first: this.firstName.val,
+        last: this.lastName.val,
+        desc: this.description.val,
+        rate: this.rate.val,
+        areas: this.areas.val
       };
       this.$emit('save-data', formData);
     }
